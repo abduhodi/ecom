c9291c9d0f1f0b0a591660c917adae1545c0cabe
Samandar Irismatillayev <syoma@MacBook-Air-Samandar-2.local>
2023-11-09 16:23:22 +0500

    added category table

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { JwtModule } from '@nestjs/jwt';
 import { Admin } from './admin/models/admin.model';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { Admin } from './admin/models/admin.model';
       models: [Admin],
     }),
     AdminModule,
+    CategoryModule,
   ],
   controllers: [],
   providers: [],
