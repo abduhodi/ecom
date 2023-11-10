a16a43f6e83f574c6e5738d2fa798c39c5fe2677
Samandar Irismatillayev <syoma@MacBook-Air-Samandar-2.local>
2023-11-10 10:01:53 +0500

    User almost complited

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Matches, IsEmail } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ description: 'The first name of the user.' })
   first_name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ description: 'The last name of the user.' })
   last_name: string;
+
+  @IsEmail()
+  @ApiProperty({ description: 'The email address of the user (optional).' })
   email?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'The check number for verification.',
+    example: '+998989909090',
+  })
+  @Matches(/^\+998\d{9}$/, { message: 'Invalid phone number format' })
   phone_number: string;
 }
--- src/user/dto/phone-for-otp.dto.ts
+++ src/user/dto/phone-for-otp.dto.ts
@@ -1,8 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class PhoneNumberDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: 'The check number for verification.',
+    example: '+998989909090',
+  })
   @Matches(/^\+998\d{9}$/, { message: 'Invalid phone number format' })
   phone_number: string;
 }
