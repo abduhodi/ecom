913c931bb5e449026f2a1870b10605e6c2722821
samandarlv <134990329+samandarlv@users.noreply.github.com>
2023-11-12 17:16:33 +0500

    added product_model

--- src/brand_category/models/brand_category.model.ts
+++ src/brand_category/models/brand_category.model.ts
@@ -2,6 +2,7 @@ import { BelongsTo, ForeignKey, HasMany, Model } from 'sequelize-typescript';
 import { Column, DataType, Table } from 'sequelize-typescript';
 import { Brand } from '../../brand/models/brand.model';
 import { Category } from '../../category/models/category.model';
+import { ProductModel } from 'src/product_model/model/product_model.model';
 
 interface BrandCategoryAttr {
   brand_id: number;
@@ -16,12 +17,17 @@ export class BrandCategory extends Model<BrandCategory, BrandCategoryAttr> {
   @Column({ type: DataType.INTEGER, allowNull: false })
   @ForeignKey(() => Brand)
   brand_id: number;
+
   @BelongsTo(() => Brand)
   brand: Brand;
 
   @Column({ type: DataType.INTEGER, allowNull: false })
   @ForeignKey(() => Category)
   category_id: number;
+
   @BelongsTo(() => Category)
   category: Category;
+
+  @HasMany(() => ProductModel)
+  models: ProductModel;
 }
