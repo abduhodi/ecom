95a8083ea15906665feef60f3a5801b429a187a7
samandarlv <134990329+samandarlv@users.noreply.github.com>
2023-11-14 17:57:44 +0500

    added filter for product

---
+++ src/product/dto/filter-product.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class FilterProductDto {
+  @ApiProperty({
+    example: { from: 50, to: 5000 },
+    description: 'It returns products between this price',
+  })
+  price: { from: number; to: number };
+
+  @ApiProperty({ example: 1, description: 'Id of model' })
+  brand_id: number;
+
+  attributes: [{ attribute_id: number; attribute_value: string }];
+}
