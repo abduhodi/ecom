2b389807339f81b897dfa59227a6c453efc7a2f3
valiev1011 <valievyunus1011@gmail.com>
2023-11-30 17:23:57 +0500

    credentials fixed

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -105,6 +105,7 @@ export class ProductController {
     @Req() req: Request,
     @Res({ passthrough: true }) res: Response,
   ) {
+    // console.log('USER_ID', Object.assign({}, req.cookies));
     return this.productService.findOne(+id, accessToken, req, res);
   }
 
